a058782d4a1dfb10e92aa6f59f9bb3109d9c9d9a
lasmon10kapota <133053264+lasmon10kapota@users.noreply.github.com>
2024-12-25 13:22:31 +0200

    Added a code for selecting qutes based on user prefererd quote category

--- src/components/GenerateQuote.jsx
+++ src/components/GenerateQuote.jsx
@@ -1,27 +1,48 @@
-import React from 'react'
-import myquote from './Quotes.json'
+import React from 'react';
+import general from './General.json';
+import family from './Family.json';
+import jugment from './Jugment.json';
+import marriage from './Marriage.json';
+import salvation from './Salvation.json';
+import victory from './Victory.json';
+import youth from './Youth.json';
+
+const categories = {
+    general,
+    youth,
+    marriage,
+    family,
+    salvation,
+    victory,
+    jugment
+};
 
 function GenerateQuote() {
-    const maxLength = myquote.length;
-    const randQuoteCount = Math.floor(Math.random() * maxLength);
-    const [count, setCount] = React.useState(randQuoteCount);
+    const [selectedCategory, setSelectedCategory] = React.useState("general");
+    const [count, setCount] = React.useState(Math.floor(Math.random() * categories[selectedCategory].length));
+
+
+    const maxLength = categories[selectedCategory].length;
+
+
+    function handleSelectedCategory(e) {
+        setSelectedCategory(e.target.value);
+        setCount(Math.floor(Math.random() * categories[e.target.value].length)); // reset count when category changes
+    }
+
 
     function getRandomQuote() {
-        const randQuoteCount = Math.floor(Math.random() * maxLength);
-        setCount(count => randQuoteCount)
-    }
-    function getNextQuote() {
-        if (count === maxLength - 1) {
-            setCount(count => 0)
-        }
-        setCount(count => count + 1)
+        setCount(Math.floor(Math.random() * maxLength));
     }
 
+
+    function getNextQuote() {
+        setCount((prevCount) => (prevCount === maxLength - 1 ? 0 : prevCount + 1));
+    }
+
+
     function getPreviousQuote() {
-        if (count === 0) {
-            setCount(count => maxLength - 1)
-        }
-        setCount(count => count - 1)
+        setCount((prevCount) => (prevCount === 0 ? maxLength - 1 : prevCount - 1));
     }
 
     return (
@@ -44,24 +65,35 @@ function GenerateQuote() {
             <div className="quote_section">
                 <hr />
                 <div className='buttons_holder'>
-                    <button onClick={getPreviousQuote}>Prev</button>
-                    <button onClick={getRandomQuote}>Get Quote</button>
-                    <button onClick={getNextQuote}>Next</button>
+                    <div className='button_style'>
+                        <select value={selectedCategory} onChange={handleSelectedCategory}>
+                            <option value="general">Choose Category</option>
+                            <option value="youth">Youth</option>
+                            <option value="marriage">Marriage</option>
+                            <option value="family">Family</option>
+                            <option value="salvation">Salvation</option>
+                            <option value="victory">Victory</option>
+                            <option value="jugment">Jugment</option>
+                        </select>
+                    </div>
+                    <button className='button_style' onClick={getPreviousQuote}>Prev</button>
+                    <button className='button_style' onClick={getRandomQuote}>Get Quote</button>
+                    <button className='button_style' onClick={getNextQuote}>Next</button>
                 </div>
+
                 <hr />
                 <div className="variable_quote_holder">
-                    <h2 style={{ textAlign: 'center', color: 'white' }}>Enyoy SOP Quotes</h2>
+                    <h2 style={{ textAlign: 'center', color: 'white' }}>Enjoy SOP Quotes</h2>
                     <hr />
-                    <p><b>{myquote[count].quotation}</b></p>
-                    <h4 style={{ color: 'darkred' }}>{myquote[count].reference}</h4>
+                    <p><b>{categories[selectedCategory][count].quotation}</b></p>
+                    <h4 style={{ color: 'darkred' }}>{categories[selectedCategory][count].reference}</h4>
                 </div>
             </div>
             <footer>
                 <h2 title="@ Lasmon Kapota; email: lsmnkapota@gmail.com">&copy; LK Soft Development 2024</h2>
             </footer>
         </div>
-
-    )
+    );
 }
 
-export default GenerateQuote
+export default GenerateQuote;
